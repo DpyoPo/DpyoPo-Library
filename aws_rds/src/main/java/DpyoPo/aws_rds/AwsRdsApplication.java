8a28246b4cddd3a882bac975d5608ff428ee7634
DESKTOP-UNJHOPU\user <c5ngsdp@gmail.com>
2024-08-08 16:06:20 +0900

    init AWS_RDS

---
+++ aws_rds/src/main/java/DpyoPo/aws_rds/AwsRdsApplication.java
@@ -0,0 +1,13 @@
+package DpyoPo.aws_rds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRdsApplication.class, args);
+	}
+
+}
