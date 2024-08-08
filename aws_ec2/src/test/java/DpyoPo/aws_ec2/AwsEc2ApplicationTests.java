b89fd2b2c049fe8d78ca3b102b6245da82a7f60e
DESKTOP-UNJHOPU\user <c5ngsdp@gmail.com>
2024-08-08 16:06:08 +0900

    init AWS_EC2

---
+++ aws_ec2/src/test/java/DpyoPo/aws_ec2/AwsEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package DpyoPo.aws_ec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
